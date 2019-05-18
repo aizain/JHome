80e61582cb53783018d9f38a27848882d24ef22e
王子豪 <964641075@qq.com>
2019-05-18 12:34:17 +0800

    Add mybatis driver project and add mysql driver

---
+++ mybatis-driver/src/main/java/com/aizain/jhome/mybatis/driver/mysql/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package com.aizain.jhome.mybatis.driver.mysql.mapper;
+
+import com.aizain.jhome.mybatis.driver.po.User;
+
+/**
+ * UserMapper
+ *
+ * @author Zain
+ * @date 2019-05-16
+ */
+public interface UserMapper {
+
+    /**
+     * 查询用户 根据ID
+     *
+     * @param id
+     * @return
+     */
+    User findUserById(Integer id);
+
+    /**
+     * 插入用户
+     *
+     * @param user
+     * @return
+     */
+    int insertUser(User user);
+
+}
