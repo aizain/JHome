b9396e505c095558a9d8e6e1a71e168d75ff879a
wangzihao <wangzihao@chinadaas.com>
2019-07-22 23:10:30 +0800

    Add adapter pattern, servlet

---
+++ design-pattern/src/main/java/com/aizain/jhome/design/pattern/adapter/servlet/HttpServletImpl.java
@@ -0,0 +1,13 @@
+package com.aizain.jhome.design.pattern.adapter.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * HttpServletImpl
+ *
+ * @author Zain
+ * @date 2019-07-22
+ */
+public class HttpServletImpl extends HttpServlet {
+
+}
---
+++ design-pattern/src/main/java/com/aizain/jhome/design/pattern/adapter/servlet/JavaxServletImpl.java
@@ -0,0 +1,23 @@
+package com.aizain.jhome.design.pattern.adapter.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * JavaxServletImpl
+ *
+ * @author Zain
+ * @date 2019-07-22
+ */
+public class JavaxServletImpl extends GenericServlet {
+
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+}
