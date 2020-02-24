022e2d4da0feeda20270cebeaf9c1cb15128b5c7
wangzihao <wangzihao@chinadaas.com>
2020-02-24 09:30:26 +0800

    Add tree order

---
+++ computer-basic/src/main/java/com/aizain/jhome/computer/data/tree/Node.java
@@ -0,0 +1,26 @@
+package com.aizain.jhome.computer.data.tree;
+
+import java.util.List;
+
+/**
+ * Node
+ *
+ * @author Zain
+ * @date 2020/2/23
+ */
+public class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
