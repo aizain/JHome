8a67d18f68859907956ef1097a9f0d0cce20a110
wangzihao <wangzihao@chinadaas.com>
2020-04-22 20:12:23 +0800

    Add RightSideView_199

---
+++ computer-basic/src/main/java/com/aizain/jhome/computer/data/tree/TreeNode.java
@@ -0,0 +1,17 @@
+package com.aizain.jhome.computer.data.tree;
+
+/**
+ * TreeNode
+ *
+ * @author Zain
+ * @date 2020/4/22
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
